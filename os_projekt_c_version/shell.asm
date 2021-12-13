972ad9b839517c7e9bdb93b560f4a9756f28b1f6
Łukasz Niedźwiadek <37691734+SunBear1@users.noreply.github.com>
2021-12-13 22:46:43 +0100

    Add files via upload

---
+++ os_projekt_c_version/shell.asm
@@ -0,0 +1,69 @@
+.686
+.model flat
+extern _MessageBoxA@16: PROC
+extern _ExitProcess@4: PROC
+public _shellcode, _get_size, _get_adr
+
+; 18,446,744,073,709,551,615
+.data
+tytul db 'TEST', 0
+
+.code
+
+_get_size proc
+
+ mov  eax,offset _end_shell
+ sub eax,offset poczatek
+ ret
+ _get_size endp
+
+_get_adr proc
+
+ ;mov  eax,offset _end_shell
+ mov eax,offset poczatek
+ ret
+ _get_adr endp
+
+ poczatek:
+_shellcode proc
+nop
+pushad
+nop
+nop
+nop
+nop
+nop
+call    routine
+
+routine:
+pop     ebp
+nop
+nop
+nop
+sub     ebp, offset routine
+push    0                               ; // MB_OK
+lea     eax, [ebp + szCaption]
+push    eax                          ;    // lpCaption
+lea     eax, [ebp + szText]
+push    eax                           ;   // lpText
+push    0                             ;   // hWnd
+mov     eax, 0AAAAAAAAh
+call    eax                            ;  // MessageBoxA
+
+popad
+push    0AAAAAAAAh                     ;  // OEP
+ret
+
+szCaption :
+db 'dddsafasdfsadfdasfdsafdfsdaf',0
+
+
+szText :
+db 'dddsafasdfsadfdasfdsafdfsdaf',0
+ nop
+
+_shellcode ENDP
+
+_end_shell: nop
+
+END
